ca48fedeb67f02eee7b42e2e8dda24e85af84c95
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2016-11-30 16:06:11 +0100

    New minor release 0.0.6

--- tf/text.py
+++ tf/text.py
@@ -20,3 +20,29 @@ class Text(object):
 
     def bookNode(self, name, lang='en'):
         return self.nodeFromName['' if lang not in self.languages else lang].get(name, None)
+
+    def passage(self, n, firstSlot=False, lang='en'):
+        F = self.api.F
+        L = self.api.L
+        slotType = F.otype.slotType
+        nType = F.otype.v(n)
+        r = L.d(n, slotType)[0] if firstSlot and nType != slotType else n
+        rType = F.otype.v(r)
+        if rType in {'book', 'chapter', 'verse'}: return r
+        isSlot = rType == slotType
+        isBook = rType == 'book'
+        isChapter = rType == 'chapter'
+        isVerse = rType == 'verse'
+        slots = [r] if isSlot else L.d(r, slotType)
+        fS = r if isSlot else slots[0]
+        lS = r if isSlot else slots[-1]
+        bn = r if isBook else  L.u(r, 'book')
+        cn = r if isChapter else None if isBook else L.u(r, 'chapter')
+        vn = r if isVerse else None if isBook or isChapter else L.u(fS, 'verse')
+        vnl = r if isVerse else None if isBook or isChapter else L.u(lS, 'verse')
+        return (
+            self.bookName(bn, lang=lang),
+            F.chapter.v(cn),
+            F.verse.v(vn),
+        )+((F.verse.v(vnl),) if vn != vnl else ())
+
