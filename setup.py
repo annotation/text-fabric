cd9a60bd12ba162bef1d3777e1517ecf8d7f3677
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2017-01-04 12:59:58 +0100

    New minor release 2.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='text-fabric',
     packages=['tf'],
-    version='2.1.0',
+    version='2.1.1',
     description='''Processor for Text Fabric Data''',
     author='Dirk Roorda',
     author_email='shebanq@ancient-data.org',
--- tf/fabric.py
+++ tf/fabric.py
@@ -8,7 +8,7 @@ from .api import *
 from .mql import MQL
 
 NAME = 'Text-Fabric'
-VERSION = '2.1.0'
+VERSION = '2.1.1'
 APIREF = 'https://github.com/ETCBC/text-fabric/wiki/Api'
 TUTORIAL = 'https://github.com/ETCBC/text-fabric/blob/master/docs/tutorial.ipynb'
 DATA = 'https://github.com/ETCBC/text-fabric-data'
