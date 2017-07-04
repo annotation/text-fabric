18c942b29406630cf090065425b886e9efa0ba7e
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2017-07-04 13:48:48 +0200

    leipzig

---
+++ presentations/Leipzig2017-07-06/semantic/semNumber.tf
@@ -0,0 +1,17 @@
+@node
+@author=J.S. Bach, Leipzig
+@source=Semantic plurality training set
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2017-07-04T11:27:53Z
+
+400518	p
+400523	p
+400536	p
+400538	s
+400544	s
+400549	p
+400553	p
+400578	s
+s
+400595	p
