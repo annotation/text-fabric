04d1463a94436e0d1e4a12e518438944160d1df7
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2018-02-27 21:00:51 +0100

    New minor release 3.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='text-fabric',
     packages=['tf'],
-    version='3.2.1',
+    version='3.2.2',
     description='''Processor for Text Fabric Data''',
     author='Dirk Roorda',
     author_email='dirk.roorda@dans.knaw.nl',
