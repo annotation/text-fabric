7dc93ba2332b325f7af6247f9349d9b2b25a420a
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2018-12-13 23:00:20 +0100

    removed trash

--- apps/bhsa/incantation.py
+++
@@ -1,3 +0,0 @@
-from tf.extra.bhsa import Bhsa
-A = Bhsa(hoist=globals())
-print(f'{F.otype.maxSlot} {F.otype.slotType}s')
--- apps/uruk/incantation.py
+++
@@ -1,3 +0,0 @@
-from tf.extra.cunei import Cunei
-A = Cunei(hoist=globals())
-print(f'{F.otype.maxSlot} {F.otype.slotType}s')
