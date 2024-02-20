907f19d3e96ac1b3319bededbfce2fc092364301
Dirk Roorda <dirk.roorda@di.huc.knaw.nl>
2024-02-20 11:08:10 +0100

    comparison STAM TF in WATM docs

--- tf/convert/watm.py
+++ tf/convert/watm.py
@@ -1,10 +1,118 @@
 """Export to Web Annotation Text Model
 
-# The general idea
+# The situation
 
 This module can export a TF corpus to WATM (Web Annotation Text Model),
 which is the input format of the suite of systems developed by Team Text for
 serving text plus annotations over the web.
+
+If we can convert TF corpora to WATM, then we have an avenue to the
+[KNAW/HuC/DI/Team-Text](https://di.huc.knaw.nl/text-analysis-en.html)
+web publishing machinery.
+
+Given the fact that TF can already convert TEI and PageXML corpora, this
+completes a pipeline from source to publication.
+
+We have done this for the following corpora:
+
+*   [`mondriaan/letters`](https://github.com/annotation/mondriaan)
+*   [`translatin/corpus`](https://gitlab.huc.knaw.nl/translatin/corpus)
+*   [`suriano/letters`](https://gitlab.huc.knaw.nl/suriano/letters)
+
+All these corpora need distinct preprocessing steps before they are "canalized" into
+TF, see the illustration below.
+
+![confluence](../images/text-confluence.jpg)
+
+At the same time,
+[Maarten van Gompel](https://github.com/proycon)
+is also making pipelines to the Team-Text publishing street. He uses his
+[STAM](https://github.com/annotation/stam)
+software to build a
+[pipeline](https://github.com/knaw-huc/brieven-van-hooft-pipeline/blob/main/README.md)
+from a corpus of letters by P.C. Hooft in Folia format to text segments and
+web annotations.
+
+# Excursion: STAM
+
+![stam](../images/stam.png)
+
+We now have two sytems,
+[STAM](https://github.com/annotation/stam)
+and Text-Fabric that can untangle text and markup.
+They are implemented very differently, and have a different flavour, but at the
+same time they share the preference of separating the textual data from all the data
+around the text.
+
+*   *intent*
+
+    *   **STAM**: make it easier for tools and infrastructure to handle texts with
+    annotations.
+    *   **TF**: support researchers in analysing textual corpora.
+
+*   *implementation*
+
+    *   **STAM**: Rust + [Python bindings](https://github.com/annotation/stam-python).
+    *   **TF**: Pure Python.
+
+*   *organization*
+
+    *   **STAM**: very neatly in a core with extensions.
+    *   **TF**: core data functionality in `tf.core` modules, search functionality in
+        `tf.search` modules, lots of other functions are included in the code with
+        varying degrees of integration and orderliness!
+
+*   *standards*
+
+    *   **STAM**: actively seeks to employ existing standards.
+    *   **TF**: relies on a few simple conventions w.r.t. data organization.
+
+*   *model*
+
+    *   **STAM**: very generic w.r.t. annotations, annotations can target
+        annotations and /or text segments.
+    *   **TF**:
+        [graph model](https://annotation.github.io/text-fabric/tf/about/datamodel.html)
+        where nodes stand for textual positions and subsets of them, nodes and
+        edges can have features, which are the raw material of annotations, but
+        annotations are not a TF concept.
+
+*   *query language*
+
+    *   **STAM**:
+        [STAMQL](https://github.com/annotation/stam/tree/master/extensions/stam-query),
+        evolving as an SQL-like language, with user-friendly primitives for
+        annotations.
+    *   **TF**:
+        [TF-Query](https://annotation.github.io/text-fabric/tf/about/searchusage.html),
+        a noise-free language for graph templates with reasonable performance.
+
+*   *display*
+
+    *   **STAM**: In development, see `stam view` in
+        [STAM tools](https://github.com/annotation/stam-tools).
+    *   **TF**: Powerful functions to display corpus fragments with highlighting in
+        `tf.advanced`. The challenge is to build generic display functions that detect
+        the peculiarities of the corpora.
+
+*   *API*
+
+    *   **STAM**: in Rust and Python.
+    *   **TF**: Python.
+
+*   *GUI*
+
+    *   **STAM**: not yet.
+    *   **TF**: locally served web interface for browsing and searching the corpus.
+
+Both libraries can be used to manage corpus data in intricate ways for research
+and publishing purposes.
+How STAM and Text-Fabric will evolve in the dynamic landscape of corpora, analytical
+methods and AI, is something we cannot predict.
+For now, their different flavour and intent will define their appeal to the different
+categories of users.
+
+# The general idea
 
 The idea of WATM is, like the idea of Text-Fabric, to untangle the text from its
 markup. Everything outside the text itself is coded in annotations.
